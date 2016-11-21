70bcd4f284e5ad9286fb6f3a4f1c2542bfc028c8
gimslab <gimslab.com@gmail.com>
2016-11-21 23:56:46 +0900

    yml test

--- src/test/java/ymltest/Test1.java
+++ src/test/java/ymltest/Test1.java
@@ -2,7 +2,9 @@ package ymltest;
 
 import java.io.File;
 import java.io.FileReader;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.junit.Test;
 
@@ -12,13 +14,33 @@ public class Test1 {
 
 	@Test
 	public void test1() throws Exception {
-		File f = new File("/home/gim/git/ymltest/src/main/java/ymltest/contract.yml");
+		File f = new File("contract.yml");
+		System.out.println(f.getCanonicalPath());
 
 		YamlReader reader = new YamlReader(new FileReader(f));
 		Object object = reader.read();
 		System.out.println(object);
 		Map map = (Map) object;
-		System.out.println(map.get("address"));
+
+		/*
+    name: Nathan Sweet
+    age: 28
+    address: 4011 16th Ave S
+    phone numbers:
+     - name: Home
+       number: 206-555-5138
+     - name: Work
+       number: 425-555-2306
+		 */
+		System.out.printf("name = %s\n", map.get("name"));
+		System.out.printf("age = %s\n", map.get("age"));
+		System.out.printf("address = %s\n", map.get("address"));
+		System.out.printf("phone numbers = \n");
+		List<Map<String, String>> nums = (List<Map<String, String>>)map.get("phone numbers");
+
+		nums.forEach(e -> {
+			System.out.printf("  name=%s, number=%s\n", e.get("name"), e.get("number"));
+		});
 	}
 
 }
